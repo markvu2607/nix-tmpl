350a048d246c8644f4488871167e4e7d69393e9e
Mark Vu <markvu2607@miava.io.vn>
2026-04-27 15:18:38 +0700

    feat: init tanstack-start-monorepo

---
+++ templates/tanstack-start-monorepo/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Bun basic";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            bun
+            vtsls
+          ];
+        };
+      }
+    );
+}
